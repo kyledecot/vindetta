89bf9dc246c1dd15c7b8be40d93525c23b369cd5
Kyle Decot <kyle@joinroot.com>
2017-12-22 09:28:35 -0500

    Replaces FactoryGirl with FactoryBot

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,9 +1,9 @@
-require "factory_girl"
+require "factory_bot"
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    FactoryGirl.find_definitions
+    FactoryBot.find_definitions
   end
 end
