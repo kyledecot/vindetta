e80ae4d3733b96875dbf6cae90d6b39e5be34635
Kyle Decot <kyle@joinroot.com>
2018-01-15 15:58:33 -0500

    inlines transliterator

--- lib/vindetta/calculator.rb
+++ lib/vindetta/calculator.rb
@@ -1,28 +1,14 @@
 module Vindetta
   class Calculator
-    CHECK_DIGITS = "0123456789X".chars
-    WEIGHTS = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
-
     def self.check_digit(vin)
-      CHECK_DIGITS[sum(vin) % CHECK_DIGITS.length]
-    end
-
-    private
-
-    def self.transliterate(vin)
       mapping = "0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ".chars
-
-      vin.chars.map do |c|
-        index = mapping.find_index(c)
-        raise Vindetta::InvalidCharacter, c unless index
-        index % 10
-      end
-    end
-
-    def self.sum(vin)
-      transliterate(vin)
-        .zip(WEIGHTS)
+      sum = vin
+        .chars
+        .map { |c| mapping.index(c) % 10 }
+        .zip([8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2])
         .reduce(0) { |sum, (a, b)| sum + (a * b) }
+
+      "0123456789X"[sum % 11]
     end
   end
 end
